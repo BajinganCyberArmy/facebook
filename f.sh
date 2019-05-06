89bdb43677ed04555ecea9afc0a33680506db5b9
BajinganCyberArmy <49521386+BajinganCyberArmy@users.noreply.github.com>
2019-05-06 16:28:48 +0700

    Add files via upload

---
+++ f.sh
@@ -0,0 +1,157 @@
+#!/system/xbin/bash
+#Author B4J1ⁿ64ⁿ T³RH0RM⁴T
+#thanks BCA
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+
+figlet B4J1NG4N TOOLS | lolcat
+
+echo $cyan"Hanya Satu Pilihan"
+echo $green"Tekan Nomer 1 Untuk MenginstalNya=>"
+echo $purple"[1] Facebook Brute "
+echo 
+echo $green "╭─~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+ [    Author       :    B4J1ⁿ64ⁿ T³RH0RM⁴T                                  
+ [    Daftar Tools :    13/350 Jenis                                                  
+ [    Github       :    https://github.com/B4J1NG4Nt3rh0rm4t
+ [    Message      :    Pergunakan lah Tools Ini Sebaik Mungkin 
+ [    My Team      :    B4J1NG4N Cyber Team\/DarkNess Cyber Team
+ ╰─~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>"
+
+read -p "root@B4J1ⁿ64ⁿ=•>" F47u2
+
+
+if [ $F47u2 = 1 ] || [ $F47u2 = 1 ]
+then
+clear
+pkg install sl
+sl
+sleep 1
+clear
+echo "
+\ 
+\ \ 
+\ \     ___________________________________________
+\  \  {______________My Noob Tools________________}
+\  \/_____________________________________________ 
+\   WELCOME TO B4J1NG4N T3RH0RM4T FBbrute    /
+ \_____________________________________________/"
+figlet congratulations |lolcat
+sleep 1
+echo $green" Installing fbbrute2 Cuk ..."
+sleep 10
+clear
+figlet TUNGGU 20 MENIT OK |lolcat
+sleep 5
+echo
+echo
+figlet Sabar Ya Cuk :V |lolcat
+sleep 1
+clear
+echo $green"#"
+sleep 1
+echo $red"##"
+sleep 1
+echo $blue"###"
+sleep 1
+echo $cyan"####"
+sleep 1
+echo $purple"#####"
+sleep 1
+echo $white"######"
+sleep 2
+echo $green"#######"
+echo $red"########"
+sleep 3
+echo $blue"#########"
+echo $cyan"##########"
+echo $purple"###########"
+echo $white"############"
+echo $green"#############"
+sleep 1
+echo $red"##############"
+sleep 2
+echo $blue"###############"
+sleep 3
+echo $cyan"################"
+echo $purple"#################"
+sleep 5
+echo $white"##################"
+sleep 2
+echo $green"###################"
+echo $red"####################"
+echo $blue"#####################"
+sleep 1
+echo $cyan"######################"
+echo $purple"#######################"
+echo $white"########################"
+echo $green"#########################"
+sleep 1
+echo $red"##########################"
+sleep 1
+echo $cyan"###########################"
+sleep 1
+echo $purple"############################"
+sleep 1
+echo $white"#############################"
+echo $green"##############################"
+echo $red"###############################"
+echo $cyan"################################"
+echo $purple"#################################"
+echo $white"##################################"
+echo $green"###################################"
+sleep 1
+echo $red"####################################"
+sleep 1
+echo $cyan"#####################################"
+sleep 1
+echo $purple"######################################"
+sleep 1
+echo $white"#######################################"
+sleep 1
+echo $green"########################################"
+sleep 1
+echo $red"#########################################"
+sleep 1
+echo $cyan"##########################################"
+sleep 1
+echo $purple"###########################################"
+echo $white"############################################"
+echo $green"#############################################"
+echo $red"##############################################"
+sleep 1
+echo $cyan"###############################################"
+sleep 1
+echo $purple"################################################"
+sleep 1
+echo $white"#################################################"
+echo $green"##################################################"
+sleep 1
+echo $red"###################################################"
+sleep 1
+echo $cyan"####################################################"
+echo $purple"#####################################################"
+sleep 1
+echo $white"######################################################"
+sleep 1
+echo $green"#######################################################"
+sleep 5
+clear
+figlet Finishing Sayang | lolcat
+sleep 1
+apt-get update && apt-get upgrade -y
+apt-get install git -y 
+git clone https://github.com/BajinganCyberArmy/fbbrute2
+mv fbbrute2 $HOME
+cd $HOME/fbbrute2
+clear
+sleep 1
+cd fbbrute2
+python2 f.py
+fi
